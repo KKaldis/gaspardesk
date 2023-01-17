666df2d384cdc7c4ec7ea7455f4bc08b2cb07bda
Kostas Kaldis <kostaskaldis@gmail.com>
2023-01-17 17:53:21 +0200

    fetch testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import NavMenu from "./NavMenu";
 import PostCard from "./PostCard";
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      const response = await fetch("www-test.gaspardesk.com/cms/api/articles?populate=*", {
+        method: "GET"
+      });
+      try {
+        const json = await response.json();
+        setData(json);
+        console.log(data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+
+    }
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <div className="App-Layout">

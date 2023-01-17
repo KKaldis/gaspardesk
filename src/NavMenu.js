e70e949cde5ae2a6f08415047faf1bb9e3f63897
Kostas Kaldis <kostaskaldis@gmail.com>
2023-01-17 13:39:00 +0200

    dropdown

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -4,22 +4,29 @@ import navarrow from "./assets/navarrow.svg";
 import "./NavMenu.scss";
 
 const NavMenu = () => {
-    return (
-        <div className="menu-container">
-            <img src={logo} alt="logo" />
-            <div className="button-wrapper">
-                <div className="button-group">
-                    <div className="button-outline">Platform
-                        <img src={navarrow} />
-                    </div>
-                    <div className="button-outline">Solutions</div>
-                    <div className="button-outline">Benefits</div>
-                    <div className="button-outline">Blog</div>
-                </div>
-                <div className="button-solid">Request a Demo</div>
+  return (
+    <div className="menu-container">
+      <img src={logo} alt="logo" />
+      <div className="button-wrapper">
+        <div className="button-group">
+          <div className="button-dropdown">
+            <div className="button-outline">
+              Platform
+              <img src={navarrow} />
             </div>
+            <ul>
+              <li className="button-outline">Integrations</li>
+              <li className="button-outline">Pricing</li>
+            </ul>
+          </div>
+          <div className="button-outline">Solutions</div>
+          <div className="button-outline">Benefits</div>
+          <div className="button-outline">Blog</div>
         </div>
-    );
+        <div className="button-solid">Request a Demo</div>
+      </div>
+    </div>
+  );
 };
 
 export default NavMenu;

b5e493a797f417bdd0b60614fe42b91265dea903
Constantine <kostaskaldis@gmail.com>
2023-01-18 21:47:21 +0200

    lazy import for blog

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Footer from "./layout/Footer";
 import NavMenu from "./layout/NavMenu";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "./components/ui/Loader";
-import Blog from "./Blog";
 
 const App = () => {
   const Landing = React.lazy(() => import("./Landing"));
+  const Blog = React.lazy(() => import("./Blog"));
 
   return (
     <Router>

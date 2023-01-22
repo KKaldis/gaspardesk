c42dc51b2c07423387524a6691fa8953775ed410
Kostas Kaldis <kostaskaldis@gmail.com>
2023-01-22 03:26:20 +0200

    refactoring

--- src/components/animationSequence.js
+++ src/components/animationSequence.js
@@ -3,134 +3,78 @@ import { Elastic, gsap } from "gsap";
 const animation = () => {
   const item1 = gsap.timeline({ repeat: -1, repeatDelay: 0 });
   const item2 = gsap.timeline({ repeat: -1, repeatDelay: 1.5 });
-  item1.from("#anim-item-1", 0, {
+
+  const top = {
+    y: "-200",
+    delay: 1,
+    scale: 0,
+    duration: 0.5,
+    ease: "Power1.easeInOut",
+  };
+
+  const bottom = {
+    y: "200",
+    delay: 1,
+    scale: 0,
+    duration: 0.5,
+    ease: "Power1.easeInOut",
+  };
+
+  item1.from("#anim-item-1", {
+    duration: 0,
     delay: 0,
     scale: 0,
     ease: "Power1.easeInOut",
   });
-  item1.to("#anim-item-1", 0.5, {
-    y: "-200",
-    delay: 1,
-    ease: "Power1.easeInOut",
-    scale: 0,
-  });
+  item1.to("#anim-item-1", top);
 
-  item2.from("#anim-item-2", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item2.to("#anim-item-2", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item2.from("#anim-item-2", bottom);
+  item2.to("#anim-item-2", top);
 
-  item1.from("#anim-item-3", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item1.to("#anim-item-3", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item1.from("#anim-item-3", bottom);
+  item1.to("#anim-item-3", top);
 
-  item2.from("#anim-item-4", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item2.to("#anim-item-4", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item2.from("#anim-item-4", bottom);
+  item2.to("#anim-item-4", top);
 
-  item1.from("#anim-item-5", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item1.to("#anim-item-5", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item1.from("#anim-item-5", bottom);
+  item1.to("#anim-item-5", top);
 
-  item2.from("#anim-item-6", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item2.to("#anim-item-6", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item2.from("#anim-item-6", bottom);
+  item2.to("#anim-item-6", top);
 
-  item1.from("#anim-item-7", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item1.to("#anim-item-7", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item1.from("#anim-item-7", bottom);
+  item1.to("#anim-item-7", top);
 
-  item2.from("#anim-item-8", 0.5, {
-    y: "200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
-  item2.to("#anim-item-8", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item2.from("#anim-item-8", bottom);
+  item2.to("#anim-item-8", top);
 
-  item1.from("#anim-item-9", 0.5, {
-    y: "200",
+  item1.from("#anim-item-9", {
     fill: "var(--light)",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
+    ...bottom
   });
-  item1.to("#anim-item-9", 0, {
+  item1.to("#anim-item-9", {
     fill: "var(--light)",
+    duration: 0,
     delay: 0,
     ease: "Power1.easeInOut",
   });
 
-  item1.from("#anim-item-9", 0, {
+  item1.from("#anim-item-9", {
     fill: "var(--light)",
+    duration: 0,
     delay: 0,
     ease: "Power1.easeInOut",
   });
-  item1.to("#anim-item-9", 0.5, {
+  item1.to("#anim-item-9", {
+    duration: 0.5,
     rotate: "-90",
     fill: "var(--primary)",
     delay: 1,
     ease: "Power1.easeInOut",
   });
-  item1.to("#anim-item-9", 0.5, {
+  item1.to("#anim-item-9", {
+    duration: 0.5,
     x: "200",
     y: "-200",
     delay: 0.5,
@@ -138,7 +82,8 @@ const animation = () => {
     display: "none",
   });
 
-  item2.from("#anim-item-10", 0.5, {
+  item2.from("#anim-item-10", {
+    duration: 0.5,
     x: "-200",
     y: "200",
     delay: 2,
@@ -146,20 +91,17 @@ const animation = () => {
     ease: Elastic.easeOut.config(1, 0.3),
   });
 
-  item2.to("#anim-item-10", 0.5, {
-    y: "-200",
-    delay: 1,
-    scale: 0,
-    ease: "Power1.easeInOut",
-  });
+  item2.to("#anim-item-10", top);
 
-  item1.from("#anim-item-11", 0.5, {
+  item1.from("#anim-item-11", {
+    duration: 0.5,
     x: "-200",
     delay: 1,
     scale: 0,
     ease: "Power1.easeInOut",
   });
-  item1.to("#anim-item-11", 0, {
+  item1.to("#anim-item-11", {
+    duration: 0,
     scale: 0,
     delay: 1.5,
     ease: "Power1.easeInOut",

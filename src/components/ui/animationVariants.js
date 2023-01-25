b17669b2a2a6fc12f5ff95f0053d89242a18cb9a
Kostas Kaldis <kostaskaldis@gmail.com>
2023-01-25 04:28:34 +0200

    transition effect

--- src/components/ui/animationVariants.js
+++ src/components/ui/animationVariants.js
@@ -1,6 +1,14 @@
 export const transitionVariants = {
-  out: { opacity: 0, transform: "rotate3d(10, 39, 5, -90deg)" },
-  in: { opacity: 1, transform: "rotate3d(0, 0, 0, 0deg)" },
+  out: {
+    opacity: 0,
+    transform: "rotate3d(10, 39, 5, -90deg)",
+    filter: "drop-shadow(100vw -100vh 0px var(--primary-opacity))",
+  },
+  in: {
+    opacity: 1,
+    transform: "rotate3d(0, 0, 0, 0deg)",
+    filter: "drop-shadow(0px 0px 0px transparent)",
+  },
 };
 
 export const pageTransitions = {
